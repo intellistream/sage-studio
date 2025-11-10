c0fcebe9e3a66d9455607e98efbe2db684bf2d8e
Shuhao Zhang <shuhao_zhang@hust.edu.cn>
2025-11-10 12:04:45 +0800

    Revert "chore: bump version to 0.1.9.0 [version bump]"
    This reverts commit c4cf65ddfd2285e564f646d0b2bce0441fc24557.

--- src/sage/studio/_version.py
+++ src/sage/studio/_version.py
@@ -1,6 +1,6 @@
 """Version information for sage-studio package."""
 
 # 独立硬编码版本
-__version__ = "0.1.9.0"
+__version__ = "0.1.8.0"
 __author__ = "IntelliStream Team"
 __email__ = "shuhao_zhang@hust.edu.cn"
